47f9471fcea6baff8239b4fb72e13a8a80b3e421
Roberto <roby.men2a@gmail.com>
2022-10-19 22:53:54 -0400

    Conversion a Componentes

--- practica02/componentes/producto.js
+++ practica02/componentes/producto.js
@@ -1,10 +1,76 @@
 Vue.component(
-    'producto-vue',
+    'producto-selec',
     {
         template:
             `
-           
+            <div class='container'>
+            
+            <div class='row'>
+                <h3>{{nombre}}</h3>
+            </div>
+            <div class='row'>
+                <div class='col-12 col-sm-6 col-md-4 '>
+                    <img v-bind:src='imagen' alt='' width='100%'>
+                </div>
+                <div class='col-12 col-sm-6  col-md-8'>
+                    <h6>{{descripcion}}</h6>
+                    <div class='p-3 mb-2 text-white' v-bind:style='precioestilos'>
+                        Precio: {{precio}} BOB
+                    </div>
+                    <h5>Color</h5>
+                    <div>                        
+                        <div class='color-box clic' v-for='color of colores' @click='asignarColor(color, id, nombre)' v-bind:style='color'></div>
+                    </div>
+                    <h5>Cantidad</h5>
+                    <div class='quantity'>
+                        <button v-on:click='restarCantidad()'>-</button>
+                        <div> {{counter}}</div> <button v-on:click='addCantidad()'>+</button>
+                    </div>
+                    <div class='buy-box' v-if='counter<=0'>
+                        <button class='btn btn-primary' disabled>Comprar</button>
+                    </div>
+                    <div class='buy-box' v-else>
+                        <button class='btn btn-primary' v-on:click='comprar()'>Comprar</button>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
         `,
-        props:["productoSeleccionado"]
+        props: ['id', 'nombre', 'imagen', 'descripcion', 'precio', 'colores', 'precioestilos'],
+        data: function () {
+            return {
+                counter: 0
+                ,
+                pedido: [
+                    {
+                        id: null,
+                        cantidad: 0,
+                        color: null,
+                        nombre: null
+                    }
+                ]
+            }
+        },
+        methods: {
+            addCantidad: function () {
+                this.counter += 1;
+            },
+            restarCantidad: function () {
+                if (this.counter > 0) {
+                    this.counter -= 1;
+                }
+            },
+            comprar: function () {
+                this.pedido.cantidad = this.counter;
+                alert('Esta seguro de comprar el producto id: ' + this.pedido.id + " - " + this.pedido.nombre + ", una cantidad de " + this.pedido.cantidad);
+            }, asignarColor: function (color, id, nombre) {
+                this.pedido.id = id;
+                this.pedido.color = color;
+                this.pedido.nombre = nombre;
+            }
+
+        },
     }
 );
--- practica02/componentes/productos_card.js
+++ practica02/componentes/productos_card.js
@@ -3,36 +3,30 @@ Vue.component(
     {
         template:
             `
-            <div class='container '>
-
-            <div class='row'>
-                <h4>Productos relacionados</h4>
-            </div>
-            <div class='row'>
-                <div class='col' v-for='prod of productos'>
-
-                    <div class='card' style='width: 18rem;' @click='getProductoById(prod.id)'>
                         <div class='card-body'>
-                            <h5 class='card-title'>{{prod.nombre}}</h5>
-                            <img v-bind:src='prod.imagen' alt='' width='100%'>
-                            <p class='card-text'>{{prod.descripcion}}</p>
-                            <div class='producto-relacionado-precio'> Precio: {{prod.precio}} BOB</div>
+                            <h5 class='card-title'>{{nombre}}</h5>
+                            <img v-bind:src='imagen' alt='' width='100%'>
+                            <p class='card-text'>{{descripcion}}</p>
+                            <div class='producto-relacionado-precio'> Precio: {{precio}} BOB</div>
                             <div>
                                 <div>
-                                    <div class='color-box clic' v-for='color of prod.colores' @click=''                                        
-                                        v-bind:style='color'></div>
+                                    <div class='color-box clic' v-for='(color, index) of colores' @click=''                                        
+                                    v-bind:style='color'></div>                                        
                                 </div>
                             </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
+                        </div>                    
         `,
-        props:["productos"],
-        mounted: function(){
-            console.log("Listado  *********");            
+        //props: ['nombre', 'imagen', 'descripcion', 'precio', 'colores'],
+        props: {
+            nombre: String, imagen: String, descripcion: String, precio: String
+            , colores: {
+                type: [Array]                
+            }
+        },
+        mounted: function () {
+            console.log("Listado  de  colores*********");
+            console.log(this.colores);
+
         }
     }
 );
